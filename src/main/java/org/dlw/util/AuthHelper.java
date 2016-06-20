353bb1fc4755b56cfb43ae9c886a6e366ac6ce86
David L. Whitehurst <david@ciwise.com>
2016-06-20 10:12:22 -0400

    initial import

---
+++ src/main/java/org/dlw/util/AuthHelper.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright (c) 2016 David L. Whitehurst
+ *
+ * Permission is hereby granted, free of charge, to any person 
+ * obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package org.dlw.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
+import java.util.Calendar;
+import java.util.List;
+
+import net.oauth.jsontoken.JsonToken;
+import net.oauth.jsontoken.JsonTokenParser;
+import net.oauth.jsontoken.crypto.HmacSHA256Signer;
+import net.oauth.jsontoken.crypto.HmacSHA256Verifier;
+import net.oauth.jsontoken.crypto.SignatureAlgorithm;
+import net.oauth.jsontoken.crypto.Verifier;
+import net.oauth.jsontoken.discovery.VerifierProvider;
+import net.oauth.jsontoken.discovery.VerifierProviders;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.codec.binary.Base64;
+import org.bson.types.ObjectId;
+import org.dlw.transport.TokenInfo;
+import org.joda.time.DateTime;
+
+import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
+
+/**
+ * @author <a href="mailto:david@ciwise.com">David L. Whitehurst</a>
+ *
+ */
+public class AuthHelper {
+
+    private static final String AUDIENCE = "NotReallyImportant";
+
+    private static final String ISSUER = "ModusBox";
+
+    private static final String SIGNING_KEY = "LongAndHardToGuessValueWithSpecialCharacters@^($%*$%";
+
+    /**
+     * Creates a json web token which is a digitally signed token that contains a payload (e.g. userId to identify 
+     * the user). The signing key is secret. That ensures that the token is authentic and has not been modified.
+     * Using a jwt eliminates the need to store authentication session information in a database.
+     * @param userId
+     * @param durationDays
+     * @return
+     */
+    public static String createJsonWebToken(String userId, Long durationDays)    {
+        //Current time and signing algorithm
+        Calendar cal = Calendar.getInstance();
+        HmacSHA256Signer signer;
+        try {
+            signer = new HmacSHA256Signer(ISSUER, null, SIGNING_KEY.getBytes());
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Configure JSON token
+        JsonToken token = new net.oauth.jsontoken.JsonToken(signer);
+        token.setAudience(AUDIENCE);
+        token.setIssuedAt(new org.joda.time.Instant(cal.getTimeInMillis()));
+        token.setExpiration(new org.joda.time.Instant(cal.getTimeInMillis() + 1000L * 60L * 60L * 24L * durationDays));
+
+        //Configure request object, which provides information of the item
+        JsonObject request = new JsonObject();
+        request.addProperty("userId", userId);
+
+        JsonObject payload = token.getPayloadAsJsonObject();
+        payload.add("info", request);
+
+        try {
+            return token.serializeAndSign();
+        } catch (SignatureException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Verifies a json web token's validity and extracts the user id and other information from it. 
+     * @param token
+     * @return
+     * @throws SignatureException
+     * @throws InvalidKeyException
+     */
+    public static TokenInfo verifyToken(String token)  
+    {
+        try {
+            final Verifier hmacVerifier = new HmacSHA256Verifier(SIGNING_KEY.getBytes());
+
+            VerifierProvider hmacLocator = new VerifierProvider() {
+
+                @Override
+                public List<Verifier> findVerifier(String id, String key){
+                    return Lists.newArrayList(hmacVerifier);
+                }
+            };
+            VerifierProviders locators = new VerifierProviders();
+            locators.setVerifierProvider(SignatureAlgorithm.HS256, hmacLocator);
+            net.oauth.jsontoken.Checker checker = new net.oauth.jsontoken.Checker(){
+
+                @Override
+                public void check(JsonObject payload) throws SignatureException {
+                    // don't throw - allow anything
+                }
+
+            };
+            //Ignore Audience does not mean that the Signature is ignored
+            JsonTokenParser parser = new JsonTokenParser(locators,
+                    checker);
+            JsonToken jt;
+            try {
+                jt = parser.verifyAndDeserialize(token);
+            } catch (SignatureException e) {
+                throw new RuntimeException(e);
+            }
+            JsonObject payload = jt.getPayloadAsJsonObject();
+            TokenInfo t = new TokenInfo();
+            String issuer = payload.getAsJsonPrimitive("iss").getAsString();
+            String userIdString =  payload.getAsJsonObject("info").getAsJsonPrimitive("userId").getAsString();
+            if (issuer.equals(ISSUER) && !StringUtils.isBlank(userIdString))
+            {
+            	t.setUser(userIdString);
+                t.setIssued(new DateTime(payload.getAsJsonPrimitive("iat").getAsLong()).toString());
+                t.setExpires(new DateTime(payload.getAsJsonPrimitive("exp").getAsLong()).toString());
+                return t;
+            }
+            else
+            {
+                return null;
+            }
+        } catch (InvalidKeyException e1) {
+            throw new RuntimeException(e1);
+        }
+    }
+
+    public static String decodeBase64encoded(String encoded) {
+    	byte[] decoded = Base64.decodeBase64(encoded);
+    	String retStr = null;
+		try {
+			retStr = new String(decoded, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return retStr;
+    }
+    
+    public static void main(String[] args) {
+    	String answer = AuthHelper.decodeBase64encoded("ZGF2aWQ6c3dlZXRwZWE=");
+    	System.out.println(answer);
+    }
+}
+
